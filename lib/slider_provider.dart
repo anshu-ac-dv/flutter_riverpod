dac6228ecda01bde2a076bce56aff06e4449f608
anshu <anshu.ac.dv@gmail.com>
2026-01-28 09:51:00 +0530

    Initial Commit

--- lib/slider_provider.dart
+++ lib/slider_provider.dart
@@ -4,3 +4,16 @@ final sliderProvider = StateProvider<double>((ref) {
   return 0.0;
 });
 
+class AppState {
+  double sliderValue;
+  bool showPassword;
+
+  AppState({required this.sliderValue, required this.showPassword});
+
+  AppState copyWith({double? sliderValue, bool? showPassword}) {
+    return AppState(
+      sliderValue: sliderValue ?? this.sliderValue,
+      showPassword: showPassword ?? this.showPassword,
+    );
+  }
+}
