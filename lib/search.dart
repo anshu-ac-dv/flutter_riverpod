c1ce5c5d7c47489bdc45e2af86259f66760abf9f
anshu <anshu.ac.dv@gmail.com>
2026-01-29 17:18:57 +0530

    Initial Commit

--- lib/search.dart
+++ lib/search.dart
@@ -7,6 +7,7 @@ class Search extends ConsumerWidget {
   const Search({super.key});
 
   @override
+  // Creating a build method for the class
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
@@ -26,24 +27,39 @@ class Search extends ConsumerWidget {
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              onChanged: (value){
+              // Creating a onChanged method for the text field
+              onChanged: (value) {
+                // Calling the search method for the class and passing the value as an argument
                 ref.watch(searchProvider.notifier).search(value);
               },
             ),
           ),
-          Consumer(builder: (context, ref, child){
-            final SearchProvider = ref.watch(searchProvider);
-            return Text(SearchProvider.search);
-          }),
-          Consumer(builder: (context, ref, child){
-            final SearchProvider = ref.watch(searchProvider);
-            return Switch(
-              value: SearchProvider.isLoading,
-              onChanged: (value){
-                ref.watch(searchProvider.notifier).isLoading(value);
-              },
-            );
-          })
+          // Creating a Consumer widget for the text field
+          Consumer(
+            builder: (context, ref, child) {
+              // Calling the watch method for the class and passing the query as an argument
+              final SearchProvider = ref.watch(searchProvider);
+              // Returning the text field with the value of the query
+              return Text(SearchProvider.search);
+            },
+          ),
+          // Creating a Consumer widget for the switch
+          Consumer(
+            builder: (context, ref, child) {
+              // Calling the watch method for the class and passing the query as an argument
+              final SearchProvider = ref.watch(searchProvider);
+              // Returning the switch with the value of the query
+              return Switch(
+                // Creating a onChanged method for the switch
+                value: SearchProvider.isLoading,
+                // Creating a onChanged method for the switch
+                onChanged: (value) {
+                  // Calling the isLoading method for the class and passing the value as an argument
+                  ref.watch(searchProvider.notifier).isLoading(value);
+                },
+              );
+            },
+          ),
         ],
       ),
     );
