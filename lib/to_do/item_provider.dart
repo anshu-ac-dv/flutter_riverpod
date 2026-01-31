8548280020c8d81d49ee48f5fd0b6394c4119ef8
anshu <anshu.ac.dv@gmail.com>
2026-01-31 14:18:38 +0530

    Initial Commit

---
+++ lib/to_do/item_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_tutorial/to_do/item.dart';
+
+final itemProvider = StateNotifierProvider<ItemNotifier, List>((ref){
+  return ItemNotifier();
+});
+
+class ItemNotifier extends StateNotifier<List<Item>>{
+  ItemNotifier() : super([]);
+}
