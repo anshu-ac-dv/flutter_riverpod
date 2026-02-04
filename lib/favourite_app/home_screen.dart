14129b3845b06070746c381bd1e89b1595ccf604
anshu <anshu.ac.dv@gmail.com>
2026-02-04 17:06:12 +0530

    Initial Commit

--- lib/favourite_app/home_screen.dart
+++ lib/favourite_app/home_screen.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_tutorial/favourite_app/favourite_provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final favouriteList = ref.watch(favouriteStateProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favourite App'),
         centerTitle: true,
         backgroundColor: Colors.purpleAccent,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: favouriteList.filterdItems.length,
+              itemBuilder: (context, index) {
+                final item = favouriteList.filterdItems[index];
+                return ListTile(
+                  title: Text(item.name),
+                  trailing: Icon(
+                    item.isFavourite ? Icons.favorite : Icons.favorite_border,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(favouriteStateProvider.notifier).addItem();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
