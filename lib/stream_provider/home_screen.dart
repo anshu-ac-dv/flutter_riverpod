e2a79994901806210966e79cceef44a0ba413f74
anshu <anshu.ac.dv@gmail.com>
2026-02-06 18:40:41 +0530

    Initial Commit

---
+++ lib/stream_provider/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen5 extends ConsumerWidget {
+  const HomeScreen5({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
