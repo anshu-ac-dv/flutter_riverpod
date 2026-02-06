d7b5edd4d7bfedc1eb14522068f51bea91a70fdb
anshu <anshu.ac.dv@gmail.com>
2026-02-06 18:59:59 +0530

    Initial Commit

--- lib/stream_provider/home_screen.dart
+++ lib/stream_provider/home_screen.dart
@@ -7,19 +7,23 @@ class HomeScreen5 extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final provider = ref.watch(stockPriceProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stream Provider'),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Center(
-        child: provider.when(
-          data: (price) => Text(price.toStringAsFixed(2).toString()),
-          error: (error, stack) => Text(error.toString()),
-          loading: () => const CircularProgressIndicator(),
-        ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final provider = ref.watch(stockPriceProvider);
+          return Center(
+            child: provider.when(
+              data: (price) => Text(price.toStringAsFixed(2).toString()),
+              error: (error, stack) => Text(error.toString()),
+              loading: () => const CircularProgressIndicator(),
+            ),
+          );
+        },
       ),
     );
   }
